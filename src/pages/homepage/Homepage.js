04710f3dcaa326649178ff49205bc91b42e1f715
Erolinda Kajtazi <erolindakajtazi@gmail.com>
2023-06-23 00:18:25 +0200

    homepage

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -2,6 +2,7 @@ import "./Homepage.scss";
 
 const Homepage = () => {
   return (
+    
     <div className="homepage-container">
       <h1 className="message">Welcome to Movie Listing Application</h1>
       <p className="message-desc">Here you van find the best movies</p>
